751a86eab208e57e140f97407a10bfb65b0bbb51
darshithv <darshithv3392@gmail.com>
2024-03-05 23:00:11 +0530

    for & while loop problem

---
+++ loops/loops.java
@@ -0,0 +1,24 @@
+package loops;
+
+public class loops {
+  public static void main(String[] args) {
+    /* (for loop)
+    
+    for (int i = 0; i < 5; i++) {
+      System.out.println("Hello world");
+      
+      (while loop)
+    
+      int i = 0;
+      while(i < 5){
+        System.out.println("Hello world");
+        i++;
+      }
+    } */
+
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(i * 2);
+    }
+
+  }
+}
