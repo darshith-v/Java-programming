22999e506820689e99fc7aa951426ddc2cee401a
darshithv <darshithv3392@gmail.com>
2024-03-07 22:49:18 +0530

    removed strings input & output

--- variables/Variables.java
+++ variables/Variables.java
@@ -8,9 +8,6 @@ public class Variables {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    String name = sc.nextLine();
-    System.out.println(name);
-
     int a = sc.nextInt();
     int b = sc.nextInt();
 
