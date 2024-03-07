589ad9e259d3db2a0d1e237b657a76447fa61f7d
darshithv <darshithv3392@gmail.com>
2024-03-07 22:53:39 +0530

    methods

---
+++ methods/method.java
@@ -0,0 +1,14 @@
+package methods;
+
+public class method {
+
+  static int myMethod(int x, int y) {
+    return x + y;
+  }
+
+
+  public static void main(String[] args){
+    int solution = myMethod(7, 9);
+    System.out.println(solution);
+  }
+}
