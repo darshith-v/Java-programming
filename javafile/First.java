5334256402b08a2e6f946842a2fd7329bcd0fe8c
darshithv <darshithv3392@gmail.com>
2024-04-11 22:35:31 +0530

    Modified code

--- javafile/First.java
+++ javafile/First.java
@@ -4,15 +4,24 @@ import java.util.*;
 
 public class First {
 
+  /*
   public static void printName(String name) {
     System.out.println(name);
     return;
+  } 
+   */
+  public static int calculateSum(int a, int b) {
+    int sum = a + b;
+    return sum;
   }
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String name = sc.next();
 
-    printName(name);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    int result = calculateSum(a, b);
+    System.out.println(result);
   }
 }
