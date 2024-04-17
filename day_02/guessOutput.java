db9bc43b80d083b45496b3dabe337dd8ec462ae1
darshithv <darshithv3392@gmail.com>
2024-04-17 20:23:33 +0530

    Guesses the Output in java program

---
+++ day_02/guessOutput.java
@@ -0,0 +1,13 @@
+package day_02;
+
+public class guessOutput {
+  public static void main(String[] args) {
+    int x = 1, y = 1;
+    while (x++ <= 3) {
+      while (y++ <= 4) {
+
+      }
+    }
+    System.out.print(x+" "+y);
+  }
+}
