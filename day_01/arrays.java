e9ea52055be23349eb1750e6647ba8add12caf5e
darshithv <darshithv3392@gmail.com>
2024-04-18 01:05:40 +0530

    arrays in java

--- day_01/arrays.java
+++ day_01/arrays.java
@@ -7,15 +7,22 @@ public class arrays {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+
     int[] arr = new int[5];
+    
     arr[0] = 10;
     arr[1] = 20;
     arr[2] = 30;
     arr[3] = 40;
     arr[4] = 50;
 
-    for(int i = 0; i < arr.length; i++){
-      System.out.print(arr[i] + " ");
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = sc.nextInt();
     }
+
+    // for (int i = 0; i < arr.length; i++) {
+    //   System.out.println(arr[i] + " ");
+    // }
+    System.out.println(Arrays.toString(arr));
   }
 }
