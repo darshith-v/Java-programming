19aada8bb1f5ebbbbb4768b9ada3c1bf40c46455
darshithv <darshithv3392@gmail.com>
2024-04-20 20:11:16 +0530

    Sum of array in java

---
+++ day_05/sumOfArray.java
@@ -0,0 +1,22 @@
+package day_05;
+
+import java.util.Scanner;
+
+public class sumOfArray {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter the size of Array");
+    int arr[] = new int[sc.nextInt()];
+    int sum = 0;
+
+    System.out.println("Enter elements: ");
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = sc.nextInt();
+      sum = sum + arr[i];
+    }
+
+    System.out.println("Sum of the Array is: " + sum);
+  }
+  
+}
