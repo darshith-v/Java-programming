0321e16a981d7a193082cd7b57243e567dbe1755
darshithv <darshithv3392@gmail.com>
2024-05-03 21:59:24 +0530

    modified stack program

--- stack/stack.java
+++ stack/stack.java
@@ -1,7 +1,23 @@
 package stack;
 
+import java.util.Arrays;
+
 public class stack {
+
+  static int max = 4;
+  static int top = -1;
+  static int arr[] = new int[max];
+
   public static void main(String[] args) {
-    
+    push(1);
+    push(2);
+    push(4);
+    push(9);
+  }
+
+  static void push(int data) {
+    top = top + 1;
+    arr[top] = data;
+    System.out.println(Arrays.toString(arr));
   }
 }
