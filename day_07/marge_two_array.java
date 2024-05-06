74b7201304805eec2c560b49882d50ec700a61d0
darshithv <darshithv3392@gmail.com>
2024-05-06 22:27:14 +0530

    Marge two array in java

---
+++ day_07/marge_two_array.java
@@ -0,0 +1,27 @@
+package day_07;
+
+import java.util.Scanner;
+
+public class marge_two_array{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a[] = new int[5];
+    int b[] = new int[4];
+
+    int arr[] = new int[a.length + b.length];
+
+    for (int i = 0; i < a.length; i++) {
+      a[i] = sc.nextInt();
+      arr[i] = a[i];
+    }
+
+    for (int i = 0; i < b.length; i++) {
+      b[i] = sc.nextInt();
+      arr[i + a.length] = b[i];
+    }
+
+    for (int num : arr) {
+      System.out.print(num + " ");
+    }
+  }
+}
