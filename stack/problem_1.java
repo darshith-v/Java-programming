d3c884a1480eb15a694a36922b9af3e608e907b5
darshithv <darshithv3392@gmail.com>
2024-05-20 23:19:40 +0530

    stack_arr[0] as the top element

--- stack/problem_1.java
+++ stack/problem_1.java
@@ -1,12 +1,36 @@
 package stack;
 
 public class problem_1 {
+  
+  static int MAX = 5;
+
+  static int stack_arr[] = new int[MAX];
+  static int first = -1;
+
   public static void main(String[] args) {
-    int MAX = 5;
+    push(10);
+    push(20);
+    push(30);
+    print();
+  }
 
-    int stack_arr[] = new int[MAX];
-    int first = -1;
-
+  static void push(int data) {
+    int i = 0;
+    first += 1;
+    for (i = first; i > 0; i--) {
+      stack_arr[i] = stack_arr[i - 1];
+    }
+    stack_arr[0] = data;
+  }
+  
+  static void print() {
+    if (first == -1) {
+      System.out.println("Stackover loaded");
+      System.exit(1);
+    }
     
+    for (int i = 0; i <= first; i++) {
+      System.out.println(stack_arr[i]);
+    }
   }
 }
