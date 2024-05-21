89ebf886b0afce87553364a48c770b07fc1567d7
darshithv <darshithv3392@gmail.com>
2024-05-21 23:35:00 +0530

    commented question

--- stack/problems/delete_element.java
+++ stack/problems/delete_element.java
@@ -1,5 +1,7 @@
 package stack.problems;
 
+// write a program for implement a stack in an array stack_arr[] using  stack_arr[0] as the top element
+
 public class delete_element {
   public static void main(String[] args) {
     
