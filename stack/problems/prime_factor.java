c3f106ce30fcc669273dc4a0495d56d92cb39b5f
darshithv <darshithv3392@gmail.com>
2024-07-04 06:54:42 +0530

    modified code

--- stack/problems/prime_factor.java
+++ stack/problems/prime_factor.java
@@ -1,17 +1,35 @@
 package stack.problems;
 
+import java.util.Scanner;
+
 public class prime_factor {
-  static int max = 5;
+  static int max = 100;
 
   static int first = -1;
   static int stack_arr[] = new int[max];
+
   public static void main(String[] args) {
-    push(10);
+    System.out.println("Please enter the positive number");
+    Scanner sc = new Scanner(System.in);
+
+    int number = sc.nextInt();
+    prime_fac(number);
   }
   
   static void push(int data) {
     first += 1;
     stack_arr[first] = data;
+  }
+  
+  static void prime_fac(int num) {
+    int i = 2;
+    while (i != 1) {
+      while (num % i == 0) {
+        push(i);
+        num = num / i;
+      }
+      i++;
+    }
     
   }
 }
