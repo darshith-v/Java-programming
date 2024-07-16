9f34791e94e722388875b2701cbe8478f3d53e3d
darshithv <darshithv3392@gmail.com>
2024-07-16 19:40:16 +0530

    Modified java files

---
+++ linked_list/LL.java
@@ -0,0 +1,15 @@
+package linked_list;
+
+public class LL {
+
+  class Node {
+    String data;
+    Node next;
+
+    Node(String data) {   //constructor
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+}
