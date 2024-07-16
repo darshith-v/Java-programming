0d108ae11697ba1eb57b886ab0268198268f507a
darshithv <darshithv3392@gmail.com>
2024-07-16 23:43:44 +0530

    added Linked-List addfirst and addlast functions

--- linked_list/LL.java
+++ linked_list/LL.java
@@ -1,15 +1,66 @@
 package linked_list;
 
+
 public class LL {
+
+  Node head;
 
   class Node {
     String data;
     Node next;
 
-    Node(String data) {   //constructor
+    Node(String data) { //constructor
       this.data = data;
       this.next = null;
     }
+  }
+
+  public void addFirst(String data) {
+    Node newNode = new Node(data);
+    if (head == null) {
+      head = newNode;
+      return;
+    }
+
+    newNode.next = head;
+    head = newNode;
+  }
+
+  public void addLast(String data) {
+    Node newNode = new Node(data);
+    if (head == null) {
+      head = newNode;
+      return;
+    }
+
+    Node currNode = head;
+    while (currNode.next != null) {
+      currNode = currNode.next;
+    }
+    currNode.next = newNode;
+  }
+
+  public void printList() {
+    if (head == null) {
+      System.out.println("List is empty: ");
+    }
+
+    Node currNode = head;
+    while (currNode != null) {
+      System.out.print(currNode.data + " -> ");
+      currNode = currNode.next;
+    }
+    System.out.println("NULL");
+  }
+  
+  public static void main(String[] args) {
+    LL list = new LL();
+    list.addFirst("a");
+    list.addFirst("is");
+    list.printList();
+
+    list.addLast("list");
+    list.printList();
   }
 
 }
